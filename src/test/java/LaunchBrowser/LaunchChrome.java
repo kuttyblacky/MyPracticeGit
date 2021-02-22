2f1dd3a1f7a64e437b65f843e1c50a1ffd75708a
manikandanramesh@mail.com <manikandanramesh@mail.com>
2021-02-22 23:35:10 +0530

    Hello GitHub included

--- src/test/java/LaunchBrowser/LaunchChrome.java
+++
@@ -1,7 +0,0 @@
-package LaunchBrowser;
-
-public class LaunchChrome {
-
-
-
-}
---
+++ src/test/java/Sample/HelloWorld.java
@@ -0,0 +1,15 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+@Test
+public class HelloWorld {
+
+
+public void PrintHello() {
+	
+	System.out.println("Hello GitHub");
+}
+	
+	
+}
