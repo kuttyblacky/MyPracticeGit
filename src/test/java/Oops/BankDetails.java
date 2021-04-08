38c28a7398a9733705e44268ac2927eb97199100
manikandanramesh@mail.com <manikandanramesh@mail.com>
2021-04-09 00:23:47 +0530

    Constructor Added

---
+++ src/test/java/Oops/BankDetails.java
@@ -0,0 +1,13 @@
+package Oops;
+
+public class BankDetails {
+
+	String Name;
+	int		age;
+	int		Sno;
+	
+	public BankDetails() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/Oops/ConstrutorDecl.java
@@ -0,0 +1,16 @@
+package Oops;
+
+public class ConstrutorDecl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BankDetails bd=new BankDetails();
+		
+		System.out.println(bd.Name);
+		System.out.println(bd.age);
+		System.out.println(bd.Sno);
+	
+	}
+
+}
